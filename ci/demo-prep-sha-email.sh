d757ffd02fa8cb6d3fa0e774bb19b2704a386f80
Goutam Tadi <gtadi+nalex+jaoki+csloan+g2c@pivotal.io>
2016-06-17 12:29:08 -0700

    added

--- ci/demo-prep-sha-email.sh
+++ ci/demo-prep-sha-email.sh
@@ -5,5 +5,6 @@ SHA=$(shasum $file_to_hash | cut -d ' ' -f1)
 output_body_file=$output_body_file
 output_subject_file=$output_subject_file
 echo $output_body_file
+echo `pwd`
 echo -e "Email resource demo on $(date)" > $output_subject_file
 echo -e "SHA1 Hash of \"$file_to_hash\" is $SHA\n\nCheers!\n" > $output_body_file
