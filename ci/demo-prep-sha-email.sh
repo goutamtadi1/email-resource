2ec54667a05da95a0cf3b00e16d6055a489a0a38
Goutam Tadi <gtadi+nalex+jaoki+csloan+g2c@pivotal.io>
2016-06-17 12:32:22 -0700

    added

--- ci/demo-prep-sha-email.sh
+++ ci/demo-prep-sha-email.sh
@@ -2,8 +2,8 @@
 
 SHA=$(shasum $file_to_hash | cut -d ' ' -f1)
 
-output_body_file=$output_body_file
-output_subject_file=$output_subject_file
+output_body_file=email-out/$output_body_file
+output_subject_file=email-out/$output_subject_file
 echo $output_body_file
 echo `pwd`
 echo -e "Email resource demo on $(date)" > $output_subject_file
